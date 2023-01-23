90e7662d13b19ccef2aefd0d9525092fdc7e0b1c
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-01-23 09:30:36 -0500

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -4,67 +4,73 @@ import Confetti from 'react-confetti';
 
 export default function App() {
   let { width, height } = useWindowSize();
-  const words = ['cousin', 'mentor', 'teacher', 'developer', 'friend', 'role model', 'genius'];
+  const words = ['mentor', 'teacher', 'smart person', 'role model', 'genius'];
   const [text, setText] = useState(words[0]);
   const [currentIndex, setCurrentIndex] = useState(0);
-	const [confetties, setConfetties] = useState([
-		<Confetti
-        confettiSource = {{
-          x: 0,
-          y: height / 3,
-          w: 1,
-          h: 30
-        }}
-        wind = { .02 }
-				numberOfPieces = { 100 }
-      />,
-      <Confetti
-        confettiSource = {{
-          x: width,
-          y: height / 3,
-          w: 1,
-          h: 30
-        }}
-        wind = { -.02 }
-				numberOfPieces = { 100 }
-      />
-	]);
+  const [confetties, setConfetties] = useState([
+    <Confetti
+      confettiSource={{
+        x: 0,
+        y: height / 3,
+        w: 1,
+        h: 30,
+      }}
+      wind={0.02}
+      numberOfPieces={100}
+    />,
+    <Confetti
+      confettiSource={{
+        x: width,
+        y: height / 3,
+        w: 1,
+        h: 30,
+      }}
+      wind={-0.02}
+      numberOfPieces={100}
+    />,
+  ]);
   return (
     <div>
       {confetties}
       <button
-        className='fixed top-20 left-1/2 -translate-x-1/2 font-bold text-9xl z-30 hover:scale-125 transition-transform focus-visible:outline-none focus:outline-none'
+        className="fixed top-20 left-1/2 -translate-x-1/2 font-bold text-9xl z-30 hover:scale-125 transition-transform transition-200 focus-visible:outline-none focus:outline-none active:scale-100"
         onClick={(e) => {
           const r = () => {
             const v = Math.floor(Math.random() * words.length);
             return v === currentIndex ? r : v;
-          }
+          };
           setCurrentIndex(r());
           setText(words[currentIndex]);
-					setConfetties([
-						...confetties,
-						<Confetti
-							confettiSource = {{
-								x: Math.random() * width,
-								y: Math.random() * height,
-								w: 1,
-								h: 30,
-							}}
-							numberOfPieces = { 20 }
-							wind = { .02 }
-      			/>
-					]);
+          setConfetties([
+            ...confetties,
+            <Confetti
+              confettiSource={{
+                x: Math.random() * width,
+                y: Math.random() * height,
+                w: 1,
+                h: 30,
+              }}
+              numberOfPieces={20}
+              wind={0.02}
+            />,
+          ]);
         }}
       >
-        🎉<br/><div className = 'text-sm'>click</div>
+        🎉
+        <br />
+        <div className="text-sm">click</div>
       </button>
-      <div className='flex h-screen justify-center items-center text-3xl text-center'>
+      <div className="flex h-screen justify-center items-center text-3xl text-center">
         <div>
-          Happy birthday to <span className="font-bold text-5xl underline">Patrick</span>
-          {/* <br/>
-          an awesome <span className = 'inline-block p-2 border-red-500 border-[3px] bg-amber-500'>{ text }</span> */}
+          Happy birthday to{' '}
+          <span className="font-bold text-5xl underline">Sara</span>
+          <br />
+          an awesome{' '}
+          <span className="inline-block p-2 border-red-500 border-[3px] bg-amber-500">
+            {text}
+          </span>
         </div>
       </div>
     </div>
-  )
+  );
 }
